cfe4dff68446e41dc04e6e3c6b7d5d14b04eb1e8
KhatirShahidi <khatirshahidi02@gmail.com>
2024-09-05 17:43:02 +0800

    idk

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SideNav from './sideBar.jsx'; // Confirm correct import
+import SideNav from './sideBar'; // Confirm correct import
 import { Outlet } from 'react-router-dom'; // Outlet to render child routes
 
 function Layout() {
