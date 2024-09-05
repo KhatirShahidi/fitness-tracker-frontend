43c0e6629a74800fe173e82e876f3c6727c66948
KhatirShahidi <khatirshahidi02@gmail.com>
2024-09-05 17:37:26 +0800

    there is still a path or case sensitivity issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import SideNav from './sideBar.jsx';
+import SideNav from './sideBar.jsx'; // Confirm correct import
 import { Outlet } from 'react-router-dom'; // Outlet to render child routes
 
 function Layout() {
   return (
     <div className="flex">
       <SideNav /> {/* The sidebar is always visible */}
-      <div className="flex-1 p-4 overflow-auto"> {/* Main content area */}
+      <div className="flex-1 p-4 overflow-auto">
+        {' '}
+        {/* Main content area */}
         <Outlet /> {/* Renders the matching child route */}
       </div>
     </div>
